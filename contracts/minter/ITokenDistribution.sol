a60daa2f855360bc2a96fede1d7847f82134fd85
Trevor Austin <trevor@hologram.io>
2019-10-21 10:41:21 -0500

    Remove Solidity components

--- contracts/minter/ITokenDistribution.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity ^0.4.4;
-
-
-contract ITokenDistribution {
-    function isOver() public view returns (bool);
-    function getEndTime() public view returns (uint256);
-}
--- contracts/token/IMagicBigToken.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.4.4;
-
-
-import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
-import 'openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol';
-import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
-
-contract IMagicBigToken is ERC20, Ownable, ERC20Detailed {}
